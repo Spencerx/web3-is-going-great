9ae0174d5de2935b08234e698b141d72313ec26b
Molly White <molly.white5@gmail.com>
2026-01-21 16:16:11 -0500

    Fix screenreader issue where "loading" was repeatedly announced

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,12 @@
 export default function Loader() {
   return (
     <div className="loading-wrapper">
-      <span className="loading-animation" role="alert" aria-label="Loading" />
+      <span
+        className="loading-animation"
+        role="alert"
+        aria-label="Loading"
+        aria-live="off"
+      />
     </div>
   );
 }
